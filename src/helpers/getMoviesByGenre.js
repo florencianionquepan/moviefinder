5b22f185ebf4efcc1d867ea85d83be82fcd577ca
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-04-07 17:39:34 -0300

    Create getMoviesByGenre.js

---
+++ src/helpers/getMoviesByGenre.js
@@ -0,0 +1,16 @@
+export const getMovies=async(genre)=>{
+
+    const url=`http://localhost:9090/api/v1/bootcamp/peliculas/genero/${genre}`;
+    const resp= await fetch (url);
+    const {data}= await resp.json();
+
+    const movies =data.map(movie=>({
+        id:movie.id,
+        title:movie.titulo,
+        fecha:movie.fechaCreacion,
+        calificacion:movie.calificacion,
+        imagen:movie.imagenURL
+    }))
+    console.log(movies);
+    return movies;
+}
