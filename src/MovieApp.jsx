d75218339dcf439900122a63a1655cf1f3bb1657
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-04-06 22:37:50 -0300

    Update MovieApp.jsx

--- src/MovieApp.jsx
+++ src/MovieApp.jsx
@@ -1,5 +1,8 @@
+import { GenreApp } from "./genres/GenreApp"
+
 export const MovieApp = () => {
     return <div className='min-h-[500px] flex flex-col m-5'>
     Movies App
+    <GenreApp/>
     </div>
 }
