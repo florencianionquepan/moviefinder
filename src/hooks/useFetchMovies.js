a9d540de1912b8aeb8a0596b1d94d463b53db039
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-04-07 20:15:23 -0300

    Update useFetchMovies.js

--- src/hooks/useFetchMovies.js
+++ src/hooks/useFetchMovies.js
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getMovies } from '../helpers/getMoviesByGenre';
 
 export const useFetchMovies = (genre) => {
 
     const [movies, setMovies]= useState([]);
+    const [isLoading,setIsLoading]=useState(true);
 
     const getImagesMovies=async()=>{
         const newM = await getMovies(genre);
         setMovies(newM);
+        setIsLoading(false);
     }
 
     //en modo estricto se dispara dos veces, 
@@ -18,6 +20,6 @@ export const useFetchMovies = (genre) => {
 
   return {
     movies,
-    isLoading:true
+    isLoading
   }
 }
