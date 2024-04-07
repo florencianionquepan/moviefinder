3d796f77e13aa128da4867d7a7652b815b44fa36
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-04-07 19:35:35 -0300

    Update MoviesByGenre.jsx

--- src/genres/MoviesByGenre.jsx
+++ src/genres/MoviesByGenre.jsx
@@ -1,20 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {getMovies} from '../helpers/getMoviesByGenre';
+import { MovieItem } from '../movies/MovieItem';
 
 export const MoviesByGenre=({genre})=>{
 
     //de esta forma asi solo se dispara dos veces la funcion
     //getMovies(genre);
 
+    const [movies, setMovies]= useState([]);
+
+    const getImagesMovies=async()=>{
+        const newM = await getMovies(genre);
+        setMovies(newM);
+    }
+
     //en modo estricto se dispara dos veces, 
     //sino una sola (en produccion no se usa modo estricto)
     useEffect( () => {
-        getMovies(genre);
+        getImagesMovies();
     }, [])
 
     return (
         <>
         <h5> {genre}</h5>
+        <div className="flex mb-4 mx-10">
+            {
+            movies.map( (movie) => (
+                <MovieItem 
+                key={movie.id}
+                {...movie}/>
+            ))
+            }
+        </div>
         </>
     )
 }
