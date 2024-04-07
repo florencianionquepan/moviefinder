06c3b04f75a2e625b88970582ed2fc62af7238c3
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-04-06 22:23:56 -0300

    Update AddGenre.jsx

--- src/genres/AddGenre.jsx
+++ src/genres/AddGenre.jsx
@@ -1,20 +1,24 @@
 import { useState } from "react";
 
-export const AddGenre = () => {
+export const AddGenre = ({onNewGenre}) => {
 
-    const [inputValue, setInputValue]=useState('Comedy');
+    const [inputValue, setInputValue]=useState('');
 
     const onInputChange=({target})=>{
         setInputValue(target.value)
     }
  
     const onSubmit = (event)=>{
+        //en React 18 no se renderiza el componente hasta que esta funcion termine
+        //en 17 se renderizaba dos veces por el setGenres que estaba antes y por el setInputValue
         event.preventDefault();
-        console.log(inputValue);
+        if (inputValue.trim().length<=1) return;
+        onNewGenre(inputValue);
+        setInputValue("");
     }
 
     return (
-        <form className="w-full max-w-sm" onSubmit={(event)=>onSubmit(event)}>
+        <form className="w-full max-w-sm" onSubmit={onSubmit}>
             <div className="flex items-center border-b border-purple-500 py-2">
                 <input className="w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" 
                 type="text" 
@@ -22,7 +26,8 @@ export const AddGenre = () => {
                 value={inputValue}
                 onChange={onInputChange}/>
                 <button className="flex-shrink-0 bg-purple-500 hover:bg-purple-700 
-                    border-purple-500 hover:border-purple-700 text-sm border-4 text-white py-1 px-2 rounded">
+                    border-purple-500 hover:border-purple-700 text-sm border-4 text-white 
+                    py-1 px-2 rounded">
                         Add Genre
                 </button>
             </div>
