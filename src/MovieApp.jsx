fb727f6054ec0f44435d3fc7c5f81e913d44d98b
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-05 17:03:55 -0300

    change general estructure

--- src/MovieApp.jsx
+++ src/MovieApp.jsx
@@ -1,20 +1,34 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { GenreApp } from "./genres/GenreApp"
-import { HomePage } from "./useContext/HomePage"
-import { LoginPage } from "./useContext/LoginPage"
-import { AboutPage } from "./useContext/AboutPage"
-import {MoviePage} from "./movies/MoviePage"
+import { Navigate, Route, Routes } from "react-router-dom";
+import { GenreApp } from "./genres/GenreApp";
+import { HomePage } from "./useContext/HomePage";
+import { LoginPage } from "./useContext/LoginPage";
+import { AboutPage } from "./useContext/AboutPage";
+import {MoviePage} from "./movies/MoviePage";
+import MiHeader from "./shared/MiHeader";
+import {MiFooter} from "./shared/MiFooter";
+import { UserProvider } from "./useContext/context/UserProvider";
 
 export const MovieApp = () => {
-    return <div className='min-h-[800px] flex flex-col m-5'>
-            {/* <GenreApp/> */}
-            <Routes>
-                <Route path="/" element={ <HomePage/> }/>
-                <Route path="login" element={ <LoginPage/> }/>
-                <Route path="about" element={ <AboutPage/> }/>
-                <Route path="movies" element={ <MoviePage/> }/>
-                <Route path="genres" element={ <GenreApp/> }/>
-                <Route path="/*" element={ <Navigate to="/"/> }/>
-            </Routes>
-    </div>
+    return (
+        <>
+        <UserProvider>
+            <MiHeader/>
+                <div className='min-h-[800px] flex flex-col m-5'>
+                        {/* <GenreApp/> */}
+                        <Routes>
+                            <Route path="/" element={ <HomePage/> }/>
+                            <Route path="login" element={ <LoginPage/> }/>
+                            <Route path="about" element={ <AboutPage/> }/>
+                            <Route path="movies" element={ <MoviePage/> }/>
+                            <Route path="genres" element={ <GenreApp/> }/>
+                            <Route path="/*" element={ <Navigate to="/"/> }/>
+                        </Routes>
+                </div>
+            <MiFooter/>
+        </UserProvider> 
+        </>
+    )
 }
+    
+    
+    
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MiHeader from './shared/MiHeader'
-import { MiFooter } from './shared/MiFooter'
 import './styles.css';
 import {MovieApp} from './MovieApp';
 import { BrowserRouter } from 'react-router-dom';
@@ -10,14 +8,12 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <React.StrictMode>
-            <MiHeader/>
             {/* <FormCustomHook/> */}
             {/* <MultipleCustomHook/> */}
             {/* <Layout/> */}
             {/* <Padre/> */}
             {/* <TodoApp/> */}
             <MovieApp/>
-            <MiFooter/>
         </React.StrictMode>
     </BrowserRouter>
 
