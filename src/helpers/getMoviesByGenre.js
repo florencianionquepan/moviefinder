9380da318f5d8aba35d7c86ec3160602374fa885
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-05 15:53:37 -0300

    Update getMoviesByGenre.js

--- src/helpers/getMoviesByGenre.js
+++ src/helpers/getMoviesByGenre.js
@@ -1,16 +1,22 @@
 export const getMovies=async(genre)=>{
 
     const url=`http://localhost:9090/api/v1/bootcamp/peliculas/genero/${genre}`;
-    const resp= await fetch (url);
-    const {data}= await resp.json();
+    
+    try{
+        const resp= await fetch (url);
+        const {data}= await resp.json();
 
-    const movies =data.map(movie=>({
-        id:movie.id,
-        title:movie.titulo,
-        date:movie.fechaCreacion,
-        calification:movie.calificacion,
-        image:movie.imagenURL
-    }))
-    //console.log(movies);
-    return movies;
+        const movies =data.map(movie=>({
+            id:movie.id,
+            title:movie.titulo,
+            date:movie.fechaCreacion,
+            calification:movie.calificacion,
+            image:movie.imagenURL
+        }))
+        //console.log(movies);
+        return movies;
+    }catch(error){
+        console.log("No levantaste la app");
+        return [];
+    }
 }
