6b10856ee3d94feaa1c67f4a6c517ae303b0c3f8
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-05 16:41:56 -0300

    Update MiHeader.jsx

--- src/shared/MiHeader.jsx
+++ src/shared/MiHeader.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import { FilmIcon, Bars3BottomRightIcon, XMarkIcon } from '@heroicons/react/24/solid'
+import { NavLink } from 'react-router-dom';
 
 
 export default function MiHeader() {
@@ -28,24 +29,27 @@ export default function MiHeader() {
           }
         </div>
 
-        {/* linke items */}
+        {/* lie items */}
         <ul className={`md:flex md:items-center 
         md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] 
         left-0 w-full md:w-auto md:pl-0 pl-9
         transition-all duration-500 ease-in 
         ${isOpen?'top-12':'top-[-490px]'} z-50 bg-[#21232A]`}>
           {
-            Links.map(link=>
-            <li className='font-semibold my-7 md:my-0 md:ml-8' key={link.id}>
-              <a href={link.link} className='hover:text-violet-400 duration-500'>
-                {link.name}
-              </a>
+            Links.map(li=>
+            <li className='font-semibold my-7 md:my-0 md:ml-8' key={li.id}>
+              <NavLink 
+                to={li.link}
+                // className="hover:text-violet-400 active:bg-violet-500"> 
+                className={ ({isActive}) =>`hover:text-violet-400 ${ isActive? 'text-violet-300':''}`} >
+                {li.name}
+              </NavLink>
 
             </li>)
           }
-          <button className='btn rounded bg-violet-600 font-semibold my-7 md:my-0 md:ml-8 d:static px-5 py-1'>
+          <NavLink to="login" className='btn rounded bg-violet-600 font-semibold my-7 md:my-0 md:ml-8 d:static px-5 py-1'>
             Login
-          </button>
+          </NavLink>
         </ul>
       </div>
     </div>
