df643855f283eae0a4955de6d78cafcc29ae0fb7
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-15 19:31:39 -0300

    se copian las movies que devuelve mi back para esta parte

---
+++ src/movies/data/movies.js
@@ -0,0 +1,98 @@
+export const movies=[
+    {
+        "id": 1,
+        "titulo": "Los pitufos",
+        "fechaCreacion": "1981-09-14",
+        "calificacion": 4,
+        "imagenURL": "https://pics.filmaffinity.com/the_smurfs-243073683-mmed.jpg",
+        "personajes": [
+            {
+                "id": 1,
+                "nombre": "Terry",
+                "edad": 3,
+                "peso": 4.0,
+                "historia": "Es un perro sabio que no esta vivo"
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "titulo": "Psycho",
+        "fechaCreacion": "1960-05-02",
+        "calificacion": 4,
+        "imagenURL": "https://pics.filmaffinity.com/psycho-805182707-mmed.jpg",
+        "personajes": []
+    },
+    {
+        "id": 3,
+        "titulo": "The Silence of the Lambs",
+        "fechaCreacion": "1991-10-19",
+        "calificacion": 4,
+        "imagenURL": "https://play-lh.googleusercontent.com/NN4OtSReaguunuEfZftynIypKWaDBwA-AqmWdl8JKO0UrE3izMM9qoKH3q2ToIsn7Zw",
+        "personajes": []
+    },
+    {
+        "id": 4,
+        "titulo": "Hannibal",
+        "fechaCreacion": "2001-04-10",
+        "calificacion": 5,
+        "imagenURL": "https://pics.filmaffinity.com/Hannibal-591648766-large.jpg",
+        "personajes": []
+    },
+    {
+        "id": 5,
+        "titulo": "The Matrix",
+        "fechaCreacion": "1999-04-11",
+        "calificacion": 3,
+        "imagenURL": "https://pics.filmaffinity.com/the_matrix-155050517-msmall.jpg",
+        "personajes": []
+    },
+    {
+        "id": 6,
+        "titulo": "American Beauty",
+        "fechaCreacion": "1999-06-21",
+        "calificacion": 3,
+        "imagenURL": "https://pics.filmaffinity.com/american_beauty-518516554-msmall.jpg",
+        "personajes": []
+    },
+    {
+        "id": 7,
+        "titulo": "Old",
+        "fechaCreacion": "2021-12-02",
+        "calificacion": 3,
+        "imagenURL": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT18nAyQclZRWvUYKPRPfot3PFoPVSeniH_YivSabeSxQ5l-SNq",
+        "personajes": []
+    },
+    {
+        "id": 8,
+        "titulo": "Green Book",
+        "fechaCreacion": "2018-02-02",
+        "calificacion": 5,
+        "imagenURL": "https://pics.filmaffinity.com/green_book-786419795-msmall.jpg",
+        "personajes": []
+    },
+    {
+        "id": 9,
+        "titulo": "Red",
+        "fechaCreacion": "2022-12-02",
+        "calificacion": 4,
+        "imagenURL": "https://pics.filmaffinity.com/turning_red-326602450-msmall.jpg",
+        "personajes": []
+    },
+    {
+        "id": 10,
+        "titulo": "Cars",
+        "fechaCreacion": "2006-12-02",
+        "calificacion": 4,
+        "imagenURL": "https://pics.filmaffinity.com/cars-746710621-mmed.jpg",
+        "personajes": []
+    },
+    {
+        "id": 12,
+        "titulo": "The Terminal",
+        "fechaCreacion": "2008-11-02",
+        "calificacion": 4,
+        "imagenURL": "https://play-lh.googleusercontent.com/OABiWL7T853g4zkad0Cvex507HEuTMZJTjtoVzdet9DR8vqeVbJRP6IbhQ_RL8GBZzaHxg=w240-h480-rw",
+        "personajes": []
+    }
+]
