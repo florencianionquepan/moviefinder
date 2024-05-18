c69d01d770202a5f3aaa8995f4c0278efd30fdd3
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-18 19:51:29 -0300

    Create getMoviesByName.js

---
+++ src/movies/helpers/getMoviesByName.js
@@ -0,0 +1,13 @@
+import { movies } from "../data/movies";
+
+export const getMoviesByName = (name ='') =>{
+
+    name = name.toLocaleLowerCase.trim();
+
+    if( name.length ===0 ) return [];
+
+    return movies.filter(
+        movie => movie.titulo.toLocaleLowerCase().includes( name )
+    );
+
+}
