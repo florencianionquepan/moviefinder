5e38930c97929f570295e152895929db55492b69
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-19 19:33:05 -0300

    create public and private routes

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,13 +2,31 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { LoginPage } from '../pages/LoginPage'
 import { MoviesRoutes } from './MoviesRoutes'
+import { PrivateRoute } from './PrivateRoute'
+import { PublicRouter } from './PublicRouter'
 
 export const AppRouter = () => {
   return (
     <>
         <Routes>
-            <Route path="login" element={ <LoginPage/> }/>
-            <Route path="/*" element={ <MoviesRoutes/>}/>
+          <Route path="login/*" element={
+            <PublicRouter>
+              {/* <LoginPage/> */}
+              <Routes>
+                <Route path="/*" element={<LoginPage/>}/>
+              </Routes>
+            </PublicRouter>
+          } />
+
+            {/* <Route path="login" element={ <LoginPage/> }/> */}
+
+            <Route path="/*" element={ 
+            <PrivateRoute>
+              <MoviesRoutes/> 
+            </PrivateRoute>
+            }/>
+
+            {/* <Route path="/*" element={ <MoviesRoutes/>}/> */}
         </Routes>
     </>
   )
