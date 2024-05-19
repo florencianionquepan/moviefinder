8f152446aede99e51324783fe756e97907978fd5
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-19 17:26:45 -0300

    use init of reducer and save user in local storage

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -3,23 +3,38 @@ import { AuthContext } from "./AuthContext";
 import { authReducer } from "./authReducer";
 import { types } from "../types/types";
 
-const initialState ={
+/* const initialState ={
   logged:false
+} */
+
+const init = () =>{
+  const user = JSON.parse(localStorage.getItem('user'));
+  //si 'user' no existe , user dara null
+
+  return {
+    //si user existe, logged sera true
+    logged: !!user,
+    user
+  }
 }
 
 
 export const AuthProvider = ({children}) => {
 
-  const [ state, dispatch ] = useReducer( authReducer, initialState );
+  const [ state, dispatch ] = useReducer( authReducer, {}, init );
 
   const login = (name = '')=>{
+    const user = {
+      id:1,
+      name: name
+    };
+
     const action= {
       type: types.login,
-      payload: {
-        id:1,
-        name: name
-      }
+      payload: user
     }
+
+    localStorage.setItem('user',JSON.stringify(user));
 
     dispatch(action);
   }
