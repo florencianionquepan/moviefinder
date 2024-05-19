907bbc0daf7e37a3a582c395eb79730956ce590f
Florencia Ñonquepan <85314154+florencianionquepan@users.noreply.github.com>
2024-05-19 17:11:54 -0300

    Create AuthProvider.jsx

---
+++ src/auth/context/AuthProvider.jsx
@@ -0,0 +1,35 @@
+import { useReducer, useState } from "react";
+import { AuthContext } from "./AuthContext";
+import { authReducer } from "./authReducer";
+import { types } from "../types/types";
+
+const initialState ={
+  logged:false
+}
+
+
+export const AuthProvider = ({children}) => {
+
+  const [ state, dispatch ] = useReducer( authReducer, initialState );
+
+  const login = (name = '')=>{
+    const action= {
+      type: types.login,
+      payload: {
+        id:1,
+        name: name
+      }
+    }
+
+    dispatch(action);
+  }
+
+  return (
+    <AuthContext.Provider value={{ 
+      ...state,
+      login: login
+     }}>
+        { children }
+    </AuthContext.Provider>
+  )
+}
